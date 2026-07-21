36a4178e4b98ad87afdb275158915b1e0481bc09
pupungeggang2 <kyongkyong2892@gmail.com>
2026-07-22 00:38:28 +0900

    start

---
+++ window/main.cpp
@@ -0,0 +1,55 @@
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
+
+#include <SDL2/SDL.h>
+#include <iostream>
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+bool isRunning;
+
+void loop() {
+    SDL_Event event;
+    while (SDL_PollEvent(&event) != 0) {
+        if (event.type == SDL_QUIT) {
+            isRunning = false;
+        }
+
+        #ifdef __EMSCRIPTEN__
+        if (!isRunning) {
+            emscripten_cancel_main_loop();
+        }
+        #endif
+    }
+}
+
+int main(int argc, char** argv) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL 초기화 실패: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow(
+        "SDL2 기본 윈도우", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        std::cerr << "윈도우 생성 실패: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return -1;
+    }
+
+    isRunning = true;
+
+    #ifdef __EMSCRIPTEN__
+    emscripten_set_main_loop(loop, 0, 1);
+    #else
+    while (isRunning) {
+        loop();
+        SDL_Delay(16);
+    }
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    #endif
+    return 0;
+}
+
