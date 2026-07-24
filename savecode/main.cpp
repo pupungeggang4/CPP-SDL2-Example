fbfa0c94a51063772e0475223da59f2de0af1012
pupungeggang2 <kyongkyong2892@gmail.com>
2026-07-24 15:12:29 +0900

    file system

---
+++ savecode/main.cpp
@@ -0,0 +1,84 @@
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
+
+#include <SDL2/SDL.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+bool isRunning;
+std::ifstream ifs;
+std::ofstream ofs;
+int num = 0;
+
+void loop() {
+    SDL_Event event;
+    while (SDL_PollEvent(&event) != 0) {
+        if (event.type == SDL_QUIT) {
+            isRunning = false;
+        }
+        if (event.type == SDL_MOUSEBUTTONUP) {
+            num += 1;
+        }
+
+        #ifdef __EMSCRIPTEN__
+        if (!isRunning) {
+            emscripten_cancel_main_loop();
+        }
+        #endif
+    }
+}
+
+int main(int argc, char** argv) {
+    #ifdef __EMSCRIPTEN__
+    
+    #else
+    // Init file system
+    std::string path = "";
+    path = (std::string)SDL_GetBasePath() + "save.txt";
+    ifs.open(path);
+    if (!ifs.is_open()) {
+        std::ofstream createFile(path);
+        createFile << 0;
+        createFile.close();
+        ifs.open("number.txt", std::ios::in | std::ios::out);
+    }
+    ifs >> num;
+    ifs.close();
+    #endif
+
+    // Init SDL2
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow(
+        "SDL2 기본 윈도우", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        std::cerr << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return -1;
+    }
+
+    isRunning = true;
+
+    #ifdef __EMSCRIPTEN__
+    emscripten_set_main_loop(loop, 0, 1);
+    #else
+    while (isRunning) {
+        loop();
+        SDL_Delay(16);
+    }
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    ofs.open(path);
+    ofs << num;
+    ofs.close();
+    #endif
+    return 0;
+}
+
