1f11b000a5ec560d74076b08993d81da32e77b4d
pupungeggang2 <kyongkyong2892@gmail.com>
2026-07-24 17:16:26 +0900

    temp

--- savecode/main.cpp
+++ savecode/main.cpp
@@ -9,51 +9,88 @@
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
-bool isRunning;
+bool isRunning = false;
 std::ifstream ifs;
 std::ofstream ofs;
 int num = 0;
-bool asyncLock = false;
+
+SDL_Window* window = nullptr;
+
+void run() {
+    // Init SDL2
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << SDL_GetError() << std::endl;
+        return;
+    }
+
+    window = SDL_CreateWindow(
+        "SDL2 기본 윈도우", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        std::cerr << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return;
+    }
+
+    isRunning = true;
+
+    #ifdef __EMSCRIPTEN__
+    emscripten_set_main_loop(loop, 0, 1);
+    #endif
+}
 
 #ifdef __EMSCRIPTEN__
+extern "C" {
+    EMSCRIPTEN_KEEPALIVE
+    void on_save_data_loaded() {
+        std::cout << "[C++] IndexedDB 동기화 완료. 파일 읽기를 시도합니다." << std::endl;
+        
+        ifs.open("/save/save.txt");
+        if (ifs.is_open()) {
+            ifs >> num;
+            ifs.close();
+            std::cout << "[C++] 불러온 세이브 데이터 숫자: " << num << std::endl;
+        } else {
+            std::cout << "[C++] 기존 세이브 파일이 없습니다. 0부터 시작합니다." << std::endl;
+            num = 0;
+        }
+        
+        run();
+    }
+}
+
 void emFileInit() {
     EM_ASM({
         try {
             FS.mkdir('/save');
-        } catch (e) {
-        }
+        } catch (e) {}
 
         FS.mount(FS.filesystems.IDBFS, {}, '/save');
+        console.log("IndexedDB 마운트 및 동기화 시작...");
+        
         FS.syncfs(true, function (err) {
             if (err) {
                 console.error("Failed to load save data from IndexedDB:", err);
+                _on_save_data_loaded(); 
             } else {
                 console.log("Save data loaded successfully from IndexedDB.");
+                _on_save_data_loaded(); 
             }
         });
     });
 }
 
-void emFileRead() {
-    ifs.open("/save/save.txt");
-    ifs >> num;
-    ifs.close();
-}
-
 void emFileWrite() {
     ofs.open("/save/save.txt");
-    ofs << num;
-    ofs.close();
+    if (ofs.is_open()) {
+        ofs << num;
+        ofs.close();
+    }
    
-    EM_ASM(
+    EM_ASM({
         FS.syncfs(false, function (err) {
-            if (err) {
-                console.error("Failed to persist save to IndexedDB:", err);
-            } else {
-                console.log("Save successfully persisted to IndexedDB!");
-            }
+            if (err) console.error("Failed to persist save to IndexedDB:", err);
         });
-    );
+    });
 }
 #endif
 
@@ -65,28 +102,23 @@ void loop() {
         }
         if (event.type == SDL_MOUSEBUTTONUP) {
             num += 1;
-            std::cout << num << std::endl;
-            #ifdef __EMSCRIPTEN__
-            emFileWrite();
-            #endif
+            std::cout << "현재 숫자: " << num << std::endl;
         }
-
-        #ifdef __EMSCRIPTEN__
-        if (!isRunning) {
-            emscripten_cancel_main_loop();
-        }
-        #endif
     }
+    
+    #ifdef __EMSCRIPTEN__
+    if (!isRunning) {
+        emFileWrite();
+        emscripten_cancel_main_loop();
+    }
+    #endif
 }
 
 int main(int argc, char** argv) {
     #ifdef __EMSCRIPTEN__
     emFileInit();
-    emFileRead();
     #else
-    // Init file system
-    std::string path = "";
-    path = (std::string)SDL_GetBasePath() + "save.txt";
+    std::string path = (std::string)SDL_GetBasePath() + "save.txt";
     ifs.open(path);
     if (!ifs.is_open()) {
         std::ofstream createFile(path);
@@ -96,37 +128,19 @@ int main(int argc, char** argv) {
     }
     ifs >> num;
     ifs.close();
-    #endif
 
-    // Init SDL2
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        std::cerr << SDL_GetError() << std::endl;
-        return -1;
-    }
+    run();
 
-    SDL_Window* window = SDL_CreateWindow(
-        "SDL2 기본 윈도우", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if (window == nullptr) {
-        std::cerr << SDL_GetError() << std::endl;
-        SDL_Quit();
-        return -1;
-    }
-
-    isRunning = true;
-
-    #ifdef __EMSCRIPTEN__
-    emscripten_set_main_loop(loop, 0, 1);
-    #else
     while (isRunning) {
         loop();
         SDL_Delay(16);
     }
     SDL_DestroyWindow(window);
     SDL_Quit();
+    
     ofs.open(path);
     ofs << num;
     ofs.close();
     #endif
     return 0;
 }
-
